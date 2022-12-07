2af56f8f71dec986802be9f92dce1f3cd83f71ca
Gaute Thorson Kolstad <gautekolstad@gmail.com>
2022-12-07 20:40:39 +0100

    day7

--- src/day7.py
+++ src/day7.py
@@ -1,72 +1,61 @@
-from pprint import pprint
-
-
-
 
 def main():
     with open('resources/day7.txt') as r:
         input = r.read()
 
     commands = parse(input)
+    files = findFiles(commands)
+    folders = folderSizes(files)
+
+    part1 = sum([size for size in folders.values() if size <= 100000])
+    print(part1)
+
+    minDeleteSize = 30000000 - 70000000 + folders['/']
+    part2 = min([size for size in folders.values() if size >= minDeleteSize])
+    print(part2)
 
 
-
-    pwd = '/'
-    tree = {
-        '/': [],
-    }
-
-    tree = findTree(commands, pwd, tree)
-        
-    pprint(tree)
-    
-
-    # part1 = sum([fullyContains(pair) for pair in pairs])
-    # print(part1)
-
-    # part2 = sum([overlaps(pair) for pair in pairs])
-    # print(part2)
-
-def findTree(commands, pwd, tree):
-    COM = {
-        'cd': cd,
-        'ls': ls,
-    }
-    for c in commands:
-        pwd, tree = COM[c[0][0]](c, pwd, tree)
-    return tree
-        
-    
-def cd(c, pwd, tree):
-    pwd = c[0][1]
-    return pwd, tree
-    
-def ls(c, pwd, tree):
-    for loc in c[1:]:
-        tree[pwd] = tree.get(pwd, []) + [loc]
-    return pwd, tree
+def findFiles(commands):
+    pwd = ['/']
+    files = []
+    for c, *lines in commands:
+        if c[0] == 'cd':
+            pwd = cd(c[1], pwd)
+        elif c[0] == 'ls':
+            files += ls(pwd, lines)
+    return files
 
 
-def fullyContains(pair):
-    intersect = pair[0] & pair[1]
-    if intersect in pair:
-        return 1
-    return 0
+def folderSizes(files):
+    folders = {}
+    for size, pwd in files:
+        for i in range(len(pwd)):
+            path = '/'.join(pwd[:i + 1])
+            folders[path] = folders.get(path, 0) + size
+    return folders
 
 
-def overlaps(pair):
-    if pair[0] & pair[1]:
-        return 1
-    return 0
+def cd(dir, pwd):
+    if dir == '..':
+        return pwd[:-1]
+    elif dir == '/':
+        return ['/']
+    else:
+        return pwd + [dir]
 
+
+def ls(pwd, files):
+    return [(int(size), pwd) for size, _ in files if size != 'dir']
 
 
 def parse(input):
     return [parseCommand(command_str) for command_str in input.split('$ ') if command_str]
 
+
 def parseCommand(command_str):
     command_lst = command_str.strip().split('\n')
     return [line.split(' ') for line in command_lst]
+
 
 if __name__ == '__main__':
     main()
